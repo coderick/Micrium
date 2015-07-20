5470bb322ba6849e59bec6e4e7e5dd0d3876a7c8
Rick <wolfeng123@126.com>
2015-07-20 22:08:18 +0800

    Micrium Templates and Source
    For read

---
+++ µCOS-II Project Template for Cortex-M3 on IAR/Micrium/Software/uCOS-II/Templates/IAR/Micrium/uCOS-II-Cortex-M3/includes.h
@@ -0,0 +1,84 @@
+/*
+*********************************************************************************************************
+*                                              EXAMPLE CODE
+*
+*                          (c) Copyright 2003-2013; Micrium, Inc.; Weston, FL
+*
+*               All rights reserved.  Protected by international copyright laws.
+*               Knowledge of the source code may NOT be used to develop a similar product.
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                       uC/OS-II Application Code
+*                                                Template
+*
+* The template application code provided here can be used as a foundation for your own uC/OS-II-based
+* projects.  This file, includes.h, is normally used as a master header file, containing #includes for 
+* all of an application's other headers.
+*
+* Filename      : includes.h
+* Version       : V1.00
+*********************************************************************************************************
+*/
+
+#ifndef  INCLUDES_PRESENT
+#define  INCLUDES_PRESENT
+
+
+/*
+*********************************************************************************************************
+*                                         STANDARD LIBRARIES
+*********************************************************************************************************
+*/
+
+#include  <stdarg.h>
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <math.h>
+
+
+/*
+*********************************************************************************************************
+*                                              LIBRARIES
+*********************************************************************************************************
+*/
+
+#include  <cpu.h>
+#include  <lib_def.h>
+#include  <lib_ascii.h>
+#include  <lib_math.h>
+#include  <lib_mem.h>
+#include  <lib_str.h>
+
+
+/*
+*********************************************************************************************************
+*                                              APP / BSP
+*********************************************************************************************************
+*/
+
+#include  <app_cfg.h>
+
+
+/*
+*********************************************************************************************************
+*                                                 OS
+*********************************************************************************************************
+*/
+
+#include  <ucos_ii.h>
+
+/*
+*********************************************************************************************************
+*                                            INCLUDES END
+*********************************************************************************************************
+*/
+
+
+#endif
+
+
